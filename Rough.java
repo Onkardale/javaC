0769be2c5b3f27c29864210951478bbcc35cc6f9
Onkar <onkardale20045@gmail.com>
2026-09-05 16:07:29 +0530

    javaC

--- Rough.java
+++ Rough.java
@@ -10113,6 +10113,32 @@ import java.util.Arrays;
 
 
 
+/// GCD = hcf
+///  lcm
+
+
+//
+//public class Rough {
+//    public static  void main(String[] args){
+//
+//        int num1 = 12;
+//        int num2 = 16;
+//int gcd = 0;
+//
+//
+//        for (int i = 1; i <= Math.min(num1,num2) ; i++) {
+//            if (num1 % i == 0 && num2 % i == 0){
+//            gcd = i;
+//
+//            }
+//        }
+//        int lcm = (num1 * num2)/gcd;
+//        System.out.println(lcm);
+//
+//    }
+//}
+//
+//
 
 
 
